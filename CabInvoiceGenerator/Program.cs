3bf14b75e97c730d67d2086760239fe0544c1798
PoojaAK-24 <85796760+PoojaAK-24@users.noreply.github.com>
2021-08-12 22:37:14 +0530

    Pooja Added:Main method

--- CabInvoiceGenerator/Program.cs
+++ CabInvoiceGenerator/Program.cs
@@ -6,7 +6,7 @@ namespace CabInvoiceGenerator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Cab Invoice Generator\n");
+            Console.WriteLine("*Welcome to Cab Invoice Generator\n");
 
         }
     }
